8662fa5b3004934d77b858041b55205a716af2b0
Xodzi <43913874+Xodzi@users.noreply.github.com>
2024-05-25 01:28:09 +0300

    Create StaffController.cs

---
+++ SnowBack/Controllers/StaffController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SnowBack.Models;
+
+namespace SnowBack.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly SnowmansContext _context;
+
+        public StaffController(SnowmansContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("staff/get")]
+        public async Task<List<DStaff>> Details()
+        {
+            return await _context.DStaffs.ToListAsync();
+        }
+
+        // GET: Staff/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dStaff = await _context.DStaffs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dStaff == null)
+            {
+                return NotFound();
+            }
+
+            return View(dStaff);
+        }
+
+
+        [HttpPost]
+        [Route("staff/create")]
+        public async Task<IActionResult> Create([FromBody] DStaff dStaff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dStaff);
+                await _context.SaveChangesAsync();
+                return Created();
+            }
+            return NotFound();
+        }
+
+        // GET: Staff/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dStaff = await _context.DStaffs.FindAsync(id);
+            if (dStaff == null)
+            {
+                return NotFound();
+            }
+            return View(dStaff);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surename,Lastname,Code,CallId,Phone,Email")] DStaff dStaff)
+        {
+            if (id != dStaff.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dStaff);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DStaffExists(dStaff.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dStaff);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dStaff = await _context.DStaffs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dStaff == null)
+            {
+                return NotFound();
+            }
+
+            return View(dStaff);
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dStaff = await _context.DStaffs.FindAsync(id);
+            if (dStaff != null)
+            {
+                _context.DStaffs.Remove(dStaff);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DStaffExists(int id)
+        {
+            return _context.DStaffs.Any(e => e.Id == id);
+        }
+    }
+}
