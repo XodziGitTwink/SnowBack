658ea36c600509c8b7d9afe87cc59c1146074b7d
DasolXandzo <59554571+DasolXandzo@users.noreply.github.com>
2024-06-17 16:27:45 +0300

    add getDTMCList

--- SnowBack/Controllers/TMCController.cs
+++ SnowBack/Controllers/TMCController.cs
@@ -53,10 +53,20 @@ namespace SnowBack.Controllers
             return res;
         }
 
+        // GET: /getDTMCList
+        [HttpGet]
+        [Route("getDTMCList")]
+        public async Task<List<DTMC>> GetDTMCList()
+        {
+            List<DTMC> TMCList = await _context.DTmcs.Where(x => x.IsRederved == false && x.IsUsed == false).ToListAsync();
+
+            return TMCList;
+        }
+
         // GET: /getMyTMCList
         [HttpGet]
-        [Route("getMyTMCList/{userId}")]
-        public async Task<List<DTMCModel>> GetMyTMCList(int userId)
+        [Route("getMyTMCList/{userId?}")]
+        public async Task<List<DTMCModel>> GetMyTMCList(int? userId)
         {
             List<DTMC> TMCList = await _context.DTmcs.Where(x => x.UserId == userId && x.IsRederved && x.IsUsed == false).ToListAsync();
             List<DTMCModel> myTMCList = new List<DTMCModel>();
@@ -91,5 +101,43 @@ namespace SnowBack.Controllers
 
             return myTMCList;
         }
+
+        // GET: /setReserved
+        //[HttpGet]
+        //[Route("setReserved/{userId?}/{tmcId}")]
+        //public async Task<IActionResult> SetReserved(int? userId, int tmcId)
+        //{
+        //    DTMC tmc = await _context.DTmcs.FirstOrDefaultAsync(x => x.UserId == null && x.IsRederved == false && x.IsUsed == false);
+
+        //    foreach (DTMC tmc in TMCList)
+        //    {
+        //        var shelf = await _context.DShelfs.FirstOrDefaultAsync(x => x.Id == tmc.ShelfId);
+        //        var rack = await _context.DRacks.FirstOrDefaultAsync(x => x.Id == shelf.RackId);
+        //        var room = await _context.DRooms.FirstOrDefaultAsync(x => x.Id == rack.RoomId);
+        //        var stock = await _context.DStocks.FirstOrDefaultAsync(x => x.Id == room.StockId);
+        //        myTMCList.Add(new DTMCModel
+        //        {
+        //            Id = tmc.Id,
+        //            Guid = tmc.Guid,
+        //            Name = tmc.Name,
+        //            Description = tmc.Description,
+        //            Code = tmc.Code,
+        //            Inventorycode = tmc.Inventorycode,
+        //            TypeId = tmc.TypeId,
+        //            FunctId = tmc.FunctId,
+        //            ShelfId = tmc.ShelfId,
+        //            UserId = tmc.UserId,
+        //            TaskId = tmc.TaskId,
+        //            IsRederved = tmc.IsRederved,
+        //            IsUsed = tmc.IsUsed,
+        //            StockName = stock.StockName,
+        //            RoomName = room.RoomName,
+        //            RackName = rack.RackName,
+        //            ShelfName = shelf.ShelfName
+        //        });
+        //    }
+
+        //    return myTMCList;
+        //}
     }
 }
